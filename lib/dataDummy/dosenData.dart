e6bfdcc898ae0b69329fe1510131eb153feed867
Ridwan, M.T <92246014+iwanmerdu@users.noreply.github.com>
2021-10-14 20:45:23 +0700

    Create dosenData.dart

---
+++ lib/dataDummy/dosenData.dart
@@ -0,0 +1,120 @@
+import 'package:proditi/model/dosen.dart';
+
+const dosenData=const[
+  Dosen(
+    idDosen: '01',
+    namaDosen: 'Eriawati, S.Pd., M.Pd',
+    keahlianDosen: 'Manajemen Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/photo_2021-02-23_12-54-30.jpg',
+    jabatanDosen: 'Ketua Prodi',
+    biodataSingkat: 'Eriawati, S.Pd., M.Pd merupakan ketua Program Studi Teknologi Informasi. Menempuh Pendidikan S1 pada IAIN Ar-raniry dan Pendidikan Magister di Universitas Syiah Kuala.',
+    googleScholar: 'https://scholar.google.com/citations?hl=en&user=EdCtC1AAAAAJ'),
+
+  Dosen(
+    idDosen: '02',
+    namaDosen: 'Ima Dwitawati, MBA',
+    keahlianDosen: 'Manajemen Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/e.jpg',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: 'Ima Dwitawati, MBA merupakan Dosen Biasa Prodi Teknologi Informasi. Menempuh Pendidikan S1 pada Universitas Syiah Kuala dan Pendidikan Magister di National United University.',
+    googleScholar: 'https://scholar.google.com/citations?hl=en&user=OzIeafQAAAAJ'),
+
+  Dosen(
+    idDosen: '03',
+    namaDosen: 'Hendri Ahmadian, S.Si.,M.IM',
+    keahlianDosen: 'Manajemen Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/b.jpg',
+    jabatanDosen: 'Sekretaris Prodi',
+    biodataSingkat: 'Hendri Ahmadian, S.Si.,M.IM merupakan Sekretaris Program Studi Teknologi Informasi. Menempuh Pendidikan S1 pada Universitas Syiah Kuala dan Pendidikan Magister di Chung Hua University',
+    googleScholar: 'https://scholar.google.com/citations?user=Cb9OFPIAAAAJ&hl=en&oi=ao'),
+
+  Dosen(
+    idDosen: '04',
+    namaDosen: 'Bustami, M.Sc',
+    keahlianDosen: 'Manajemen Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/photo_2021-02-23_17-12-40.jpg',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: 'Bustami, M.Sc merupakan Dosen Biasa dan ka. Lab Program Studi Teknologi Informasi. Menempuh Pendidikan S1 pada Universitas Syiah Kuala dan Pendidikan Magister di Chung Hua University',
+    googleScholar: 'https://scholar.google.co.id/citations?user=PmgkMTEAAAAJ&hl=en'),
+
+  Dosen(
+    idDosen: '05',
+    namaDosen: 'Rika Yuliana, M.T',
+    keahlianDosen: 'Manajemen Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/l.png',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+
+  Dosen(
+    idDosen: '06',
+    namaDosen: 'Khairan AR, M.Kom',
+    keahlianDosen: 'Sistem dan Teknologi Web dan Mobile',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/h.png',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+
+  Dosen(
+    idDosen: '07',
+    namaDosen: 'Andika Prajana, M.Kom',
+    keahlianDosen: 'Sistem dan Teknologi Web dan Mobile',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/j.png',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+  
+  Dosen(
+    idDosen: '08',
+    namaDosen: 'Fadhla Binti Junus, M.Sc',
+    keahlianDosen: 'Sistem dan Teknologi Web dan Mobile',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/k.jpg',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+  
+  Dosen(
+    idDosen: '09',
+    namaDosen: 'Nazaruddin Ahmad, M.T',
+    keahlianDosen: 'Sistem dan Teknologi Web dan Mobile',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/i.png',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+  
+  Dosen(
+    idDosen: '10',
+    namaDosen: 'Malahayati, M.T',
+    keahlianDosen: 'Sistem Keamanan Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/d.jpg',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+
+  Dosen(
+    idDosen: '11',
+    namaDosen: 'Mulkan Fadhli, M.T',
+    keahlianDosen: 'Sistem Keamanan Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/c.jpg',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+
+  Dosen(
+    idDosen: '12',
+    namaDosen: 'Sri Wahyuni, M.T',
+    keahlianDosen: 'Sistem Keamanan Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/g.jpg',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+  
+    Dosen(
+    idDosen: '13',
+    namaDosen: 'Ghufran Ibnu Yasa, M.T',
+    keahlianDosen: 'Sistem Keamanan Informasi',
+    gambarDosen: 'https://ti.fst.ar-raniry.ac.id/wp-content/uploads/2021/02/f.png',
+    jabatanDosen: 'Dosen Biasa',
+    biodataSingkat: '',
+    googleScholar: ''),
+];
