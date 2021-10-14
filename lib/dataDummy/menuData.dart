8c9f5c80ca4f32242ca86b3c0257a942b352b62d
Ridwan, M.T <92246014+iwanmerdu@users.noreply.github.com>
2021-10-14 20:45:54 +0700

    Create menuData.dart

---
+++ lib/dataDummy/menuData.dart
@@ -0,0 +1,27 @@
+import 'package:proditi/model/menu.dart';
+
+const menuData=const[
+  menu(
+    idMenu: '01',
+    judulMenu: 'Visi Misi',
+    gambarMenu: 'assets/images/visi.png'),
+
+  menu(
+    idMenu: '02', 
+    judulMenu: 'Sejarah Prodi', 
+    gambarMenu: 'assets/images/sejarah.png'),
+
+  menu(
+    idMenu: '03', 
+    judulMenu: 'Data Dosen',  
+    gambarMenu: 'assets/images/dosen.png'),
+
+  menu(
+    idMenu: '04', 
+    judulMenu: 'Data Mahasiswa',  
+    gambarMenu: 'assets/images/mahasiswa.png'),
+  menu(
+    idMenu: '05', 
+    judulMenu: 'Kontak',  
+    gambarMenu: 'assets/images/kontak.png'),
+];
