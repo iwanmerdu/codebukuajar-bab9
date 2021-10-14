443a2674f1763202a9099f3f75f425a9d60f370a
Ridwan, M.T <92246014+iwanmerdu@users.noreply.github.com>
2021-10-14 20:46:50 +0700

    Create menu.dart

---
+++ lib/model/menu.dart
@@ -0,0 +1,11 @@
+class menu{
+  final String idMenu;
+  final String judulMenu;
+  final String gambarMenu;
+  
+const menu({
+  required this.idMenu,
+  required this.judulMenu,
+  required this.gambarMenu
+});
+}
