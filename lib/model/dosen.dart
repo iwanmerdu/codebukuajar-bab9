5a35d1932f0fe9fd1662d8ac7bb17cbb0100abea
Ridwan, M.T <92246014+iwanmerdu@users.noreply.github.com>
2021-10-14 20:46:35 +0700

    Create dosen.dart

---
+++ lib/model/dosen.dart
@@ -0,0 +1,19 @@
+class Dosen{
+  final String idDosen;
+  final String namaDosen;
+  final String keahlianDosen;
+  final String gambarDosen;
+  final String jabatanDosen;
+  final String biodataSingkat;
+  final String googleScholar;
+  
+const Dosen({
+  required this.idDosen,
+  required this.namaDosen,
+  required this.keahlianDosen,
+  required this.gambarDosen,
+  required this.jabatanDosen,
+  required this.biodataSingkat,
+  required this.googleScholar
+});
+}
